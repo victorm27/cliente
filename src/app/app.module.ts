0a4b2b7918561aa28c1d5a2d55644d323b2200a8
victorm27 <victormanuel272791@gmail.com>
2023-05-06 17:13:03 -0500

    crud v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ProductoService } from 'src/app/services/producto.service';
+import { Producto } from '../../models/producto/producto';
+
+@Component({
+  selector: 'app-crear-producto',
+  templateUrl: './crear-producto.component.html',
+  styleUrls: ['./crear-producto.component.css']
+})
+export class CrearProductoComponent implements OnInit{
+
+  productoForm: FormGroup;
+  titulo = 'Crear Producto';
+  id:string | null;
+  constructor( private fb: FormBuilder,
+               private router: Router,
+               private toastr: ToastrService,
+               private _productoService: ProductoService,
+               private aRouter: ActivatedRoute){
+    this.productoForm = this.fb.group({
+      producto: ['',Validators.required],
+      categoria: ['',Validators.required],
+      ubicacion: ['',Validators.required],
+      precio: ['',Validators.required]
+    }
+    )
+    this.id = this.aRouter.snapshot.paramMap.get('id');
+
+  }
+
+  ngOnInit(): void {
+    this.esEditar();
+  }
+  agregarProducto(){
+
+    const PRODUCTO: Producto = {
+      nombre:this.productoForm.get('producto')?.value,
+      categoria:this.productoForm.get('categoria')?.value,
+      ubicacion:this.productoForm.get('ubicacion')?.value,
+      precio:this.productoForm.get('precio')?.value
+    }
+    if(this.id !== null){
+      //Editamos Producto
+      this._productoService.editarProducto(this.id,PRODUCTO).subscribe(data =>{
+      this.toastr.info('El producto fue actualizado con exito!', 'Producto Actualizado!');
+      this.router.navigate(['/']);
+      }, error => {
+        console.log(error);
+        this.productoForm.reset();
+      })  
+    }else{
+      //Agregamos Producto
+      console.log(PRODUCTO);
+      this._productoService.guardarProducto(PRODUCTO).subscribe(data =>{
+      this.toastr.success('El producto fue agregado con exito!', 'Producto Registrado!');
+      this.router.navigate(['/']);
+      }, error => {
+        console.log(error);
+        this.productoForm.reset();
+      })  
+    }
+  }
+
+  esEditar(){
+    if(this.id !== null){
+      this.titulo = 'Editar Producto';
+      this._productoService.obtenerProducto(this.id).subscribe(data =>{
+        this.productoForm.setValue({
+          producto: data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio,
+        })
+      })
+    }
+  }
+  editarProducto(){
+
+  }
+}
